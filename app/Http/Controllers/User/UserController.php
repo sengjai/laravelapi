abea9ecdd81d10a9e3c01cd562a9a90cddcffc23
sengjai <soonseng.ee@thetechyhub.com>
2017-06-13 18:20:15 +0800

    Inserted Cors into the files

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -16,6 +16,7 @@ class UserController extends Controller
     public function index()
     {
         $users = User::all();
+        
         return response()->json(['data' => $users], 200); 
         //200 is okay response code
 
